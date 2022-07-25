7f2488459eb36e2078ca04b8086c34af5c9f45d8
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-07-25 03:00:52 +0100

    setting up

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import {Container} from 'react-bootstrap'
+import { Store } from './Pages/Store';
+import { Home } from './Pages/Home';
+import { About } from './Pages/About';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/store" element={<Store />}/>
+        <Route path="/about" element={<About />}/>
+      </Routes>
+    </Container>
   );
 }
 
---
+++ src/Pages/Store.tsx
@@ -0,0 +1,5 @@
+export const Store = () => {
+    return (
+        <h1>Store</h1>
+    )
+}
