1c51bac24879635019b1414c7d0a1a81ab226d04
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-08-01 20:59:51 -0700

    fixed add to cart error

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,21 @@ import {Container} from 'react-bootstrap'
 import { Store } from './Pages/Store';
 import { Home } from './Pages/Home';
 import { About } from './Pages/About';
+import {Navbar} from './Components/Navbar'
+import { ShoppingCartProvider } from './context/ShoppingCartContext';
 
 const App = () => {
   return (
-    <Container>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/store" element={<Store />}/>
-        <Route path="/about" element={<About />}/>
-      </Routes>
-    </Container>
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/store" element={<Store />}/>
+          <Route path="/about" element={<About />}/>
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
   );
 }
 
--- src/Pages/Store.tsx
+++ src/Pages/Store.tsx
@@ -1,5 +1,18 @@
+import storeItems from '../Data/Items.json';
+import {Col, Row} from 'react-bootstrap';
+import { StoreItems } from '../Components/StoreItems';
+
 export const Store = () => {
     return (
-        <h1>Store</h1>
+        <>
+            <h1>Store</h1>
+            <Row md={2} xs={1} lg={3} className="g-3">
+                {storeItems.map(item => (
+                    <Col key={item.id}>
+                        <StoreItems {...item} />
+                    </Col>
+                ))}
+            </Row>
+        </>
     )
 }
